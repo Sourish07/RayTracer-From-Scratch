d2f5c269730597bd8297267f99c6974fd73bea8c
Sourish Kundu <sourishkundu07@gmail.com>
2022-07-23 11:23:54 -0700

    Refactored vector class

--- vector.py
+++ vector.py
@@ -9,9 +9,9 @@ class Vector:
         self.z = z
 
     def __add__(self, other):
-        if isinstance(other, float) or isinstance(other, int):
-            return Vector(self.x + other, self.y + other, self.z + other)
-        return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
+        if isinstance(other, Vector):
+            return Vector(self.x + other.x, self.y + other.y, self.z + other.z)            
+        return Vector(self.x + other, self.y + other, self.z + other)
 
     def __iadd__(self, other):
         self.x += other.x
@@ -20,39 +20,47 @@ class Vector:
         return self
 
     def __sub__(self, other):
-        # return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
-        return self + (-1 * other)
+        return self + (other * -1)
 
     def __isub__(self, other):
         self += (other * -1)
         return self
 
     def __mul__(self, other):
-        assert(isinstance(other, float) or isinstance(other, int))
+        if isinstance(other, Vector):
+            return Vector(self.x * other.x, self.y * other.y, self.z * other.z)
         return Vector(self.x * other, self.y * other, self.z * other)
 
     def __rmul__(self, other):
         return self * other
 
     def __imul__(self, other):
-        assert(isinstance(other, float))
-        self.x *= other.x
-        self.y *= other.y
-        self.z *= other.z
+        if isinstance(other, Vector):    
+            self.x *= other.x
+            self.y *= other.y
+            self.z *= other.z
+        else:
+            self.x *= other
+            self.y *= other
+            self.z *= other
         return self
-
-    def __itruediv__(self, other):
-        assert(isinstance(other, float))
-        self.x /= other.x
-        self.y /= other.y
-        self.z /= other.z
-        return self
-
+    
     def __truediv__(self, other):
         if isinstance(other, Vector):
             return Vector(self.x / other.x, self.y / other.y, self.z / other.z)
         assert(isinstance(other, float) or isinstance(other, int))
         return Vector(self.x / other, self.y / other, self.z / other)
+
+    def __itruediv__(self, other):
+        if isinstance(other, Vector):
+            self.x /= other.x
+            self.y /= other.y
+            self.z /= other.z
+        else:
+            self.x /= other
+            self.y /= other
+            self.z /= other
+        return self
 
     def length(self):
         return math.sqrt(self.x**2 + self.y**2 + self.z**2)
