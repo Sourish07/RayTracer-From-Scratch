2b3d2946b88950a75976765c0297afbdf091ac5e
Sourish Kundu <sourishkundu07@gmail.com>
2022-09-11 15:01:20 -0500

    Changed output path

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     for i in range(10):
         print('Rendering default scene number', i)
         scene = Scene(default_scene=i)
-        render(scene, output=f'outputs/output{i}', quality=0)
+        render(scene, output=f'output{i}', quality=0)
