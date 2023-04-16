2c0782837f2015db8350a528436e9e5fc0c4fc22
Sourish Kundu <sourishkundu07@gmail.com>
2023-04-16 17:15:10 -0500

    Added comment

--- materials/diffuse.cpp
+++ materials/diffuse.cpp
@@ -4,5 +4,6 @@ Diffuse::Diffuse(const Vector &color, const double roughness) : Material(color),
 
 Ray Diffuse::bounce(const Ray &r, const Vector &normal, const Vector &hitPos) const {
     // Normalize is called in Ray constructor
+    // Randomly choosing a point on a unit sphere and adding it to the normal scales with cosine(theta)
     return Ray(hitPos, (normal + Vector::randomUnitVector() * roughness));
 }
